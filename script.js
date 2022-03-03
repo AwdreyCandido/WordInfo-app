32005c9a600e47b93c9e50546ce403bd9e7ec870
awdrey-candido <awdrey.dev@gmail.com>
2022-03-03 19:57:45 -0300

    test new branch

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-const palavrasContainer = document.querySelector('.palavras-container')
-const botao = document.querySelector('.botao')
+const wordContainer = document.querySelector('.palavras-container')
+const btn = document.querySelector('.botao')
 
-let palavra = "";
+let word = "";
 
-const renderizarCartao = (dadosPalavra)=>{
+const renderCard = (wordInfo)=>{
 
-    let significado_2 = dadosPalavra.meanings[0].definitions[1].definition || 'No more information'
+    let word = wordInfo.word;
+    let meaning_1 = wordInfo.meanings[0].definitions[0].definition;
+    let meaning_2 = wordInfo.meanings[0].definitions[1].definition || 'No more information';
+    let phonetic = wordInfo.phonetic;
 
     let cartaoHtml = `
     <figure class="cartao">
-    <div class="nome-palavra"><span class="focus"> ${dadosPalavra.word}</span></div>
-        <div class="significado"> <strong>Definitions</strong>:  ${dadosPalavra.meanings[0].definitions[0].definition}</div>
-        <div class="significado">${significado_2}</div>
-        <div class="pronunciacao"><strong>Pronunciation</strong>:   ${dadosPalavra.phonetic}</div>
+    <div class="nome-palavra"><span class="focus"> ${word}</span></div>
+        <div class="significado"> <strong>Definitions</strong>: ${meaning_1}</div>
+        <div class="significado">${meaning_2}</div>
+        <div class="pronunciacao"><strong>Pronunciation</strong>: ${phonetic}</div>
     </figure>
     `
 
-    palavrasContainer.innerHTML = cartaoHtml
+    wordContainer.innerHTML = cartaoHtml
 }
 
-botao.addEventListener('click', ()=>{
+btn.addEventListener('click', ()=>{
 
-    palavra = document.querySelector('.input').value
+    word = document.querySelector('.input').value
 
-    const getData = async function(palavra){
-        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${palavra}`)
-        const [dadosPalavra] = await response.json()
-        renderizarCartao(dadosPalavra)
+    const getData = async function(word){
+        try {
+            const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+            const [wordInfo] = await response.json()
+            renderCard(wordInfo)
+        } catch (error) {
+            console.log(error)
+        }
     }
-    getData(palavra)
+    getData(word)
     
 })
 
